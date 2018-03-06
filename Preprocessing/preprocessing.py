f4e494760a5eca5a123bc692629e64141cc86294
Gaurav Mittal <36520345+GauravMittal20@users.noreply.github.com>
2018-03-06 15:00:54 -0800

    Add files via upload
    Categorical data and handling Missing Data

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd    #import datasets
 
 #dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('C:\Users\Gaurav Mittal\Desktop\ML\Preprocessing\Data.csv')
 X= dataset.iloc[:, :-1].values
 Y= dataset.iloc[:, 3].values
 
@@ -14,3 +14,12 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values= 'NaN',strategy ='mean', axis =0)
 imputer = imputer.fit(X[:, 1:3]) 
 X[:, 1:3]= imputer.transform(X[:, 1:3])
+
+ # Encoding Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+lablelencoder_X= LabelEncoder()
+X[:, 0] = lablelencoder_X.fit_transform(X[:, 0])
+onehotencoder= OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+lablelencoder_Y= LabelEncoder()
+Y = lablelencoder_Y.fit_transform(Y)
